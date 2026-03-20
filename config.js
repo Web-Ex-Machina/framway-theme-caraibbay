b8aac87b38196da05ed243dd61e70214c282bf9d
SeptimusVII <vansteene.quentin@gmail.com>
2026-03-20 13:10:37 +0100

    populate config

--- config.js
+++ config.js
@@ -9,6 +9,18 @@ module.exports = {
 	'secondary': 'colors(pink)',
 	'body': {
 		'font-color': 'colors(blacklight)',
+		'block-background': 'colors(white)',
+	},
+	'input': {
+		'background': 'scale-color(primary,$lightness:80%)',
+		'font-color': 'colors(white)',
+		'border-color': 'colors(none)',
+		'placeholder-font-color': 'colors(greylighter)',
+	},
+	'input-focus': {
+		'background': 'scale-color(input(background),$lightness:-10%)',
+		'font-color': 'input(font-color)',
+		'border-color': 'input(border-color)',
 	},
 	'footer': {
 		'background': 'primary'
